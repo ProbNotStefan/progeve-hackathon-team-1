5f68fc1206375fd1e7a3f12e4e76d43f1300899f
ProbNotStefan <djumaste@hotmail.com>
2025-11-19 19:51:49 +0000

    puh

--- main.py
+++ main.py
@@ -1,37 +1,52 @@
 import pygame
 import assets.data.stats as stats
-from stats import cards_id_reference
+from assets.data.stats import cards_id_reference
 from render import generate_custom_card
 
 def get_card(id):
     return cards_id_reference[id]
     
+def create_card_instance(id):
+    base_card = get_card(id)
+    return {
+        "id": id,
+        "name": base_card["name"],
+        "hp": base_card["hp"],
+        "dmg": base_card["dmg"],
+        "stars": base_card.get("stars", 0),
+        "artifacts": base_card.get("artifacts", [])
+    }
 
-player_cards = [get_card(0), get_card(1), get_card(2)]
-enemy_cards = [get_card(3), get_card(4), get_card(5)]
+player_cards = [create_card_instance(0), create_card_instance(1), create_card_instance(2)]
+enemy_cards = [create_card_instance(3), create_card_instance(4), create_card_instance(5)]
 
-def draw_card(screen, position, id):
+
+
+
+def draw_card(screen, position, card_instance):
     card_color = (255, 255, 255)
     card_rect = pygame.Rect(position[0], position[1], 100, 150)
 
-    card = f"assets/cards/live/{id}.png"
+    card_path = f"assets/cards/live/{card_instance['id']}.png"
     try:
-        card_image = pygame.image.load(card)
+        card_image = pygame.image.load(card_path)
         card_image = pygame.transform.scale(card_image, (100, 150))
         screen.blit(card_image, position)
     except:
         generate_custom_card(
-            health=str(cards_id_reference[id]["hp"]),
-            damage=str(cards_id_reference[id]["dmg"]),
-            name=cards_id_reference[id]["name"],
-            image_path=f"assets/cards/{id}.png",
-            output_filename=card
+            health=str(card_instance["hp"]),
+            damage=str(card_instance["dmg"]),
+            name=card_instance["name"],
+            image_path=f"assets/cards/{card_instance['id']}.png",
+            output_filename=card_path
         )
-        card_image = pygame.image.load(card)
+        card_image = pygame.image.load(card_path)
         card_image = pygame.transform.scale(card_image, (100, 150))
         screen.blit(card_image, position)
 
     pygame.draw.rect(screen, card_color, card_rect)
+
+
     
 
 def draw_all__cards(screen):
@@ -44,25 +59,26 @@ def draw_all__cards(screen):
         
 
 def move():
-    for idx, card in enumerate(player_cards):
-        damage = card["dmg"]
+    for idx in range(len(player_cards)-1, -1, -1):
+        card = player_cards[idx]
         if enemy_cards:
-            enemy_card = get_card(enemy_cards[idx])
-            enemy_card["hp"] -= damage
+            enemy_card = enemy_cards[min(len(enemy_cards)-1, idx)]
+            enemy_card["hp"] -= card["dmg"]
             if enemy_card["hp"] <= 0:
                 enemy_cards.pop(0)
         if card["hp"] <= 0:
             player_cards.pop(idx)
-    
-    for idx, card in enumerate(enemy_cards):
-        damage = card["dmg"]
+
+    for idx in range(len(enemy_cards)-1, -1, -1):
+        card = enemy_cards[idx]
         if player_cards:
-            player_card = get_card(player_cards[idx])
-            player_card["hp"] -= damage
+            player_card = player_cards[min(len(player_cards)-1, idx)]
+            player_card["hp"] -= card["dmg"]
             if player_card["hp"] <= 0:
                 player_cards.pop(0)
         if card["hp"] <= 0:
             enemy_cards.pop(idx)
+
 
 
 
